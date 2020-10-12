81e4a523af99b4b054b7f76bf0532058e54e3b84
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-10-12 15:13:59 +0200

    Step 2: Using AST Matchers

--- src/LoopConvert.cpp
+++ src/LoopConvert.cpp
@@ -1,9 +1,13 @@
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
 #include "llvm/Support/CommandLine.h"
+
+using namespace clang::ast_matchers;
 
 // Apply a custom category to all command-line options so that they are the only
 // ones displayed.
@@ -15,13 +19,30 @@ static llvm::cl::OptionCategory MyToolCategory("My Clang Tool");
 static llvm::cl::extrahelp
     CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
+// Matches for loops with initializers that are set to '0'
+StatementMatcher LoopMatcher =
+    forStmt(hasLoopInit(declStmt(hasSingleDecl(
+                varDecl(hasInitializer(integerLiteral(equals(0))))))))
+        .bind("forLoop");
+
+class LoopPrinter : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    if (const clang::ForStmt *FS =
+            Result.Nodes.getNodeAs<clang::ForStmt>("forLoop")) {
+      FS->dump();
+    }
+  }
+};
+
 int main(int argc, const char *argv[]) {
   clang::tooling::CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
   clang::tooling::ClangTool Tool(OptionsParser.getCompilations(),
                                  OptionsParser.getSourcePathList());
-  return Tool.run(
-      clang::tooling::newFrontendActionFactory<clang::SyntaxOnlyAction>()
-          .get());
 
-  return 0;
+  LoopPrinter Printer;
+  MatchFinder Finder;
+  Finder.addMatcher(LoopMatcher, &Printer);
+
+  return Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
 }
